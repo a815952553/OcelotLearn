7ddb5e0a210f98a05671bf6a1f42b80bfd7b25ee
wulang <wulang@567na.com>
2019-05-16 15:59:09 +0800

    基本功能实现

---
+++ Ocelot/OcelotGateWay/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.IO;
+
+namespace OcelotGateWay
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{ hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                //.AddJsonFile("ocelot.json")
+                .AddEnvironmentVariables();
+            })
+            //.ConfigureServices(s => { s.AddOcelot(); })
+            //.Configure(app => { app.UseOcelot().Wait(); })
+           .UseStartup<Startup>();
+    }
+}
